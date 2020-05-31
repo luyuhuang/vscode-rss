fe4efc258975458c6de5fc85797234699e57ce8f
luyuhuang <luyu_huang@foxmail.com>
2020-05-31 17:20:18 +0800

    using webpack

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const path = require('path');
+
+module.exports = {
+    target: 'node',
+    entry: './src/extension.ts',
+    output: {
+        path: path.resolve(__dirname, 'out'),
+        filename: 'extension.js',
+        libraryTarget: 'commonjs2',
+        devtoolModuleFilenameTemplate: '../[resource-path]',
+    },
+
+    devtool: 'source-map',
+    externals: {
+        vscode: 'commonjs vscode',
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'ts-loader'
+                    }
+                ]
+            }
+        ]
+    }
+}; 
